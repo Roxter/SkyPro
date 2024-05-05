d3010d13237e25001a3677ae250a431d1f8691d6
1 <rxmark123>
2024-05-05 20:05:12 +0700

    Homework 10.0

--- src/Main.java
+++ src/Main.java
@@ -16,8 +16,8 @@ public class Main {
         System.out.println("Данные Ф. И. О. сотрудника для заполнения отчета — " + upperFullName);
         System.out.println();
 
-        // Task 2
-        System.out.println("ЗАДАЧА 2:");
+        // Task 3
+        System.out.println("ЗАДАЧА 3:");
         fullName = "Иванов Семён Семёнович";
         String changedFullName = fullName.replace("ё", "е");
         System.out.println(changedFullName);
