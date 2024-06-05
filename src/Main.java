738bcf517b3681fa30ea5e6778ca47e7a09ae2f9
Roxter <rxmark123@gmail.com>
2024-06-05 22:56:28 +0700

    Homework 2.1.3

--- src/Main.java
+++ src/Main.java
@@ -1,12 +1,12 @@
 public class Main {
 
     public static void main(String[] args) {
-        int value = 33;
+        Integer[] value = new Integer[] {3, 4};
         changeValue(value);
-        System.out.println(value);
+        System.out.println(value[0] + " " + value[1]);
     }
 
-    private static void changeValue(Integer value) {
-        value = 22;
+    private static void changeValue(Integer[] value) {
+        value = new Integer[] {1, 2};
     }
 }
