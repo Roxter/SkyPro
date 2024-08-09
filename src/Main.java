cebf541d1895c128d2b7560fd1db5174c69a4839
Roxter <rxmark123@gmail.com>
2024-08-09 21:15:15 +0700

    HW-2.5.0

--- src/Main.java
+++ src/Main.java
@@ -1,9 +1,45 @@
+import ch.qos.logback.core.UnsynchronizedAppenderBase;
+
 public class Main {
 
     public static void main(String[] args) {
-        // Task 1
-        System.out.println("ЗАДАЧА 1:");
+        try {
+            checkParams("Gerard", "Bat_ler", "Bat_ler");
+            checkParams("Gerard", "Bat_ler", "Batler");
+            checkParams("Box", "Kubic_rounded24", "Kubic_rounded24");
+            checkParams("Box 5", "Kubic_rounded24", "Kubic_rounded2");
+        } catch (WrongLoginException w) {
+            System.out.println("Логин был введен неверно");
+        } catch (WrongPasswordException w) {
+            System.out.println("Пароль был введен неверно");
+        }
+        System.out.println("Программа завершена.");
+    }
 
-        System.out.println();
+    private static void checkParams(String login, String password, String confirmPassword) {
+        checkString(login, false);
+        checkString(password, true);
+        checkString(confirmPassword, true);
+        if (!password.equals(confirmPassword)) {
+            throw new WrongPasswordException();
+        }
+    }
+
+    private static void checkString(String string, boolean isPassword) {
+        for (int i = 0; i < string.length(); i++) {
+            char currentSymbol;
+            currentSymbol = string.charAt(i);
+            boolean isDigit = Character.isDigit(currentSymbol);
+            boolean isLetter = Character.isLetter(currentSymbol);
+            boolean isUnderscore = currentSymbol == '_';
+            boolean isOver20Symb = i >= 20;
+            if (isOver20Symb | !isDigit & !isLetter & !isUnderscore) {
+                if (!isPassword) {
+                    throw new WrongLoginException();
+                } else {
+                    throw new WrongPasswordException();
+                }
+            }
+        }
     }
 }
