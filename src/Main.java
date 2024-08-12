bd1fcf961b955f7a031f3078af6a95ffe4e67a92
ZhibarMA <rxmark123@gmail.com>
2024-08-12 18:00:41 +0700

    Homework 2.5.1

--- src/Main.java
+++ src/Main.java
@@ -1,44 +1,58 @@
-import ch.qos.logback.core.UnsynchronizedAppenderBase;
+import exception.WrongLoginException;
+import exception.WrongPasswordException;
 
 public class Main {
 
     public static void main(String[] args) {
         try {
-            checkParams("Gerard", "Bat_ler", "Bat_ler");
-            checkParams("Gerard", "Bat_ler", "Batler");
-            checkParams("Box", "Kubic_rounded24", "Kubic_rounded24");
-            checkParams("Box 5", "Kubic_rounded24", "Kubic_rounded2");
-        } catch (WrongLoginException w) {
-            System.out.println("Логин был введен неверно");
-        } catch (WrongPasswordException w) {
-            System.out.println("Пароль был введен неверно");
+            checkAuthenticate("Gerard", "Bat_ler", "Bat_ler");
+            //checkAuthenticate("Gerard", "Bat ler", "Bat ler");
+            checkAuthenticate("Gerard", "Bat_ler", "Batler");
+            checkAuthenticate("Box", "Kubic_rounded24", "Kubic_rounded24");
+            //checkAuthenticate("Box 5", "Kubic_rounded24", "Kubic_rounded2");
+        } catch (WrongLoginException wle) {
+            //System.out.println("Логин был введен неверно");
+            System.out.println(wle.getMessage());
+        } catch (WrongPasswordException wpe) {
+            //System.out.println("Пароль был введен неверно");
+            wpe.printStackTrace();
+        } finally {
+            System.out.println("Программа завершена.");
         }
-        System.out.println("Программа завершена.");
     }
 
-    private static void checkParams(String login, String password, String confirmPassword) {
-        checkString(login, false);
-        checkString(password, true);
-        checkString(confirmPassword, true);
+    private static void checkAuthenticate(String login, String password, String confirmPassword) {
+        checkLogin(login);
+        checkPassword(password);
         if (!password.equals(confirmPassword)) {
-            throw new WrongPasswordException();
+            throw new WrongPasswordException("Пароли не совпадают");
         }
     }
 
-    private static void checkString(String string, boolean isPassword) {
-        for (int i = 0; i < string.length(); i++) {
+    public static void checkLogin(String login) {
+        for (int i = 0; i < login.length(); i++) {
             char currentSymbol;
-            currentSymbol = string.charAt(i);
+            currentSymbol = login.charAt(i);
             boolean isDigit = Character.isDigit(currentSymbol);
             boolean isLetter = Character.isLetter(currentSymbol);
             boolean isUnderscore = currentSymbol == '_';
             boolean isOver20Symb = i >= 20;
-            if (isOver20Symb | !isDigit & !isLetter & !isUnderscore) {
-                if (!isPassword) {
-                    throw new WrongLoginException();
-                } else {
-                    throw new WrongPasswordException();
-                }
+            if (isOver20Symb || !isDigit && !isLetter && !isUnderscore) {
+                throw new WrongLoginException("Логин был введен неверно");
+            }
+        }
+    }
+
+    public static void checkPassword(String password) {
+        for (int i = 0; i < password.length(); i++) {
+            char currentSymbol;
+            currentSymbol = password.charAt(i);
+            boolean isDigit = Character.isDigit(currentSymbol);
+            boolean isLetter = Character.isLetter(currentSymbol);
+            boolean isUnderscore = currentSymbol == '_';
+            boolean isOver20Symb = i >= 20;
+            if (isOver20Symb || !isDigit && !isLetter && !isUnderscore) {
+                throw new WrongPasswordException("Пароль был введен неверно");
             }
         }
     }
