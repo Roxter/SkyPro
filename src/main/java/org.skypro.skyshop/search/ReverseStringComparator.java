15dbdcd4a8658fb48afa74c516fe1ba9ca77cded
Roxter <rxmark123@gmail.com>
2025-08-17 14:21:45 +0700

    Homework 2.9_2.5

---
+++ src/main/java/org.skypro.skyshop/search/ReverseStringComparator.java
@@ -0,0 +1,19 @@
+package org.skypro.skyshop.search;
+
+import java.util.Comparator;
+
+public class ReverseStringComparator implements Comparator<Searchable> {
+
+    @Override
+    public int compare(Searchable s1, Searchable s2) {
+        String name1 = s1.searchTerm();
+        String name2 = s2.searchTerm();
+        int comp = Integer.compare(name1.length(), name2.length());
+
+        if (comp != 0) {
+            return comp;
+        } else {
+            return name1.compareTo(name2);
+        }
+    }
+}
--- src/main/java/org.skypro.skyshop/search/SearchEngine.java
+++ src/main/java/org.skypro.skyshop/search/SearchEngine.java
@@ -1,6 +1,5 @@
 package org.skypro.skyshop.search;
 
-import java.util.Comparator;
 import java.util.Set;
 import java.util.TreeSet;
 
@@ -42,18 +41,3 @@ public class SearchEngine {
     }
 }
 
-class ReverseStringComparator implements Comparator<Searchable> {
-
-    @Override
-    public int compare(Searchable s1, Searchable s2) {
-        String name1 = s1.searchTerm();
-        String name2 = s2.searchTerm();
-        int comp = Integer.compare(name1.length(), name2.length());
-
-        if (comp != 0) {
-            return comp;
-        } else {
-            return name1.compareTo(name2);
-        }
-    }
-}
