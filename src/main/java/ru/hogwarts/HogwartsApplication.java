dbc65139eb9a797f96ef6e280f6934a093a4dea2
Roxter <rxmark123@gmail.com>
2025-11-21 17:53:02 +0700

    Homework 3.1.0

--- src/main/java/ru/hogwarts/HogwartsApplication.java
+++ src/main/java/ru/hogwarts/HogwartsApplication.java
@@ -1,11 +1,11 @@
-package org.skypro.skyshop;
+package ru.hogwarts;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class SkyshopApplication {
+public class HogwartsApplication {
     public static void main(String[] args) {
-        SpringApplication.run(SkyshopApplication.class, args);
+        SpringApplication.run(HogwartsApplication.class, args);
     }
 }
